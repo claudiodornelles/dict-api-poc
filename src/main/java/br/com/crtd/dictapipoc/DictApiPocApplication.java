0157c45a12ac585163f136359ef45f05f421003f
Claudio Dornelles <dornelles.crtd@gmail.com>
2024-11-02 18:03:06 -0300

    first commit

---
+++ src/main/java/br/com/crtd/dictapipoc/DictApiPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.crtd.dictapipoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictApiPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictApiPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/crtd/dictapipoc/DictApiPocApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crtd.dictapipoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictApiPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
